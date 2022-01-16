46c3fea7f43591507681d607c310880ff591fa51
Manuel-3 <58179488+Manuel-3@users.noreply.github.com>
2022-01-16 20:38:51 +0100

    Allow multiple moderator roles

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Requires a ``.env`` file in the project root directory.
 ```
 TOKEN=token
 LOG_CHANNEL=log_channel_id
-MODERATOR_ROLE=moderator_role_id
+MODERATOR_ROLES=moderator_role_id_1,moderator_role_id_2,...
 MUTED_ROLE=muted_role_id
 IDENTITY=name_or_something_to_identify_by
 ```
--- src/util.js
+++ src/util.js
@@ -1,7 +1,8 @@
 // checks if user has moderator role, uses msg to get the guild member of the user
 async function isModerator(userid, msg) {
+    const moderatorRoles = process.env.MODERATOR_ROLES.split(',');
     const member = await msg.guild.members.fetch(userid);
-    const userIsModerator = await member.roles.cache.some(r => r.id == process.env.MODERATOR_ROLE);
+    const userIsModerator = await member.roles.cache.some(r => moderatorRoles.includes(r.id));
     return userIsModerator;
 }
 
